cadf853450d18ffc4f1bb59fdf1f5849e11b5dd6
Rea Fernandes <zob06qih@rptu.de>
2026-09-02 15:39:02 +0200

    config map and redis secret

--- app/main.py
+++ app/main.py
@@ -11,10 +11,12 @@ app = FastAPI(title="Kubernetes URL Shortener")
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", "6379"))
+redis_password = os.getenv("REDIS_PASSWORD")
 
 r = redis.Redis(
     host=redis_host,
     port=redis_port,
+    password=redis_password,
     decode_responses=True
 )
 
